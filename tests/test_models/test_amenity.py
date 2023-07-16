c17531cb6333ceb3a7897df13abe820461259c35
root <root@DESKTOP-KUT53MR>
2023-07-16 23:38:49 +0000

    amenity

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" unittest for Amenity class"""
+""" unittest for the Amenity class"""
 from datetime import datetime
 from models.base_model import BaseModel
 from models.amenity import Amenity
@@ -9,17 +9,17 @@ import pep8
 
 class Test(unittest.TestCase):
     def test_for_style(self):
-        """style test"""
+        """py style test"""
         style = pep8.StyleGuide(quiet=True)
         chk = style.check_files(['models/amenity.py'])
         self.assertEqual(chk.total_errors, 0, "fix pep8")
 
     def test_docstring(self):
-        """checks for docstring"""
+        """checks for docstring documentation"""
         self.assertIsNotNone(Amenity.__doc__)
 
     def test_attributes_type(self):
-        """check that class"""
+        """check that classes"""
         my_model = BaseModel()
         self.assertIsInstance(my_model.id, str)
         self.assertIsInstance(my_model.created_at, datetime)
