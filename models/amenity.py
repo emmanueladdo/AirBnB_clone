8ba887bc28cacfd3ff6f8a0ecabd451718579748
IdiJr <idialijnr@gmail.com>
2023-07-17 14:43:28 +0000

    pycodestyle fixed

--- models/amenity.py
+++ models/amenity.py
@@ -9,7 +9,3 @@ class Amenity(BaseModel):
     """class 'Amenity' that inherits from BaseModel"""
 
     name = ""
-
-    def __init__(self, *args, **kwargs):
-        """class constructor"""
-        super().__init__(*args, **kwargs)
